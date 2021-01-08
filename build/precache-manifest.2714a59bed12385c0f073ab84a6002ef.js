d4f642970b1144bc663c916d5a12a9c851f7291c
ThabetSabha <thabetsabha98@gmail.com>
2021-01-08 16:24:53 +0200

    PWA

---
+++ build/precache-manifest.2714a59bed12385c0f073ab84a6002ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d803a4f3edcec689bcfde5de506ddf",
+    "url": "/SearchableRobots/index.html"
+  },
+  {
+    "revision": "13c7169bfda5a8222c0b",
+    "url": "/SearchableRobots/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c8471f04122836f42af8",
+    "url": "/SearchableRobots/static/css/main.4f3843c9.chunk.css"
+  },
+  {
+    "revision": "13c7169bfda5a8222c0b",
+    "url": "/SearchableRobots/static/js/2.a897ee1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SearchableRobots/static/js/2.a897ee1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8471f04122836f42af8",
+    "url": "/SearchableRobots/static/js/main.07d8f1fb.chunk.js"
+  },
+  {
+    "revision": "40b7c177607d3b153374",
+    "url": "/SearchableRobots/static/js/runtime-main.d99953dc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/SearchableRobots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
